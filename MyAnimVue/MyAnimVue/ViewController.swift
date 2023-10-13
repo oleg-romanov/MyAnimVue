defcc1a82077b5f3f4629cddf4df7b7efe50fc9b
Oleg Romanov <olegromanov.3003@gmail.com>
2023-10-13 19:13:12 +0300

    Initial commit

---
+++ MyAnimVue/MyAnimVue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAnimVue
+//
+//  Created by Олег Романов on 13.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
