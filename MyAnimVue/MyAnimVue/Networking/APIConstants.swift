952ee8a3602d383fb3ff2dc8cb73603f9ff645db
Oleg Romanov <olegromanov.3003@gmail.com>
2024-01-05 18:19:21 +0300

    Security data was hidden in the configuration file and readme was updated

--- MyAnimVue/MyAnimVue/Networking/APIConstants.swift
+++ MyAnimVue/MyAnimVue/Networking/APIConstants.swift
@@ -4,11 +4,45 @@
 //
 //  Created by Олег Романов on 13.10.2023.
 //
+import Foundation
 
 enum APIConstants {
     static let host = "https://api.anilibria.tv/v3/"
-//  static let clientId = "YourKeyHere"
-//  static let responseType = "YourSecretHere"
-    static let clientId = "apw8fl1JqlFyx0d1GBX25rciagXTUBjXk33ASb76"
     static let responseType = "token"
+    
+    func obtainClientId() -> String {
+        var myDict: [String:String]?
+        let url = obtainUrlPathToConfigPlist()
+        
+        myDict = NSDictionary(contentsOf: url) as? [String:String]
+        
+        if let clientId = myDict?["CLIENT_ID"] {
+            return clientId
+        } else {
+            fatalError("Error getting Client ID value")
+        }
+    }
+    
+    func obtainClientSecret() -> String {
+        var myDict: [String:String]?
+        let url = obtainUrlPathToConfigPlist()
+        
+        myDict = NSDictionary(contentsOf: url) as? [String:String]
+        
+        if let clientId = myDict?["CLIENT_SECRET"] {
+            return clientId
+        } else {
+            fatalError("Error getting Client Secret value")
+        }
+    }
+    
+    private func obtainUrlPathToConfigPlist() -> URL {
+        guard
+            let pathString = Bundle.main.path(forResource: "Config", ofType: "plist"),
+            let pathUrl = URL(string: pathString)
+        else {
+            fatalError("Couldn't find Config.plist in app bundle.")
+        }
+        return pathUrl
+    }
 }
